0b8cf95972c6b44e2d8bcd1b3482597472f3d8d8
Konstantin Lepeshenkov <kolepes@microsoft.com>
2021-01-17 20:39:15 +0100

    initial commit

---
+++ start-the-saga/index.ts
@@ -0,0 +1,60 @@
+﻿import * as df from "durable-functions"
+import { Context } from "@azure/functions"
+
+export default async function (context: Context): Promise<any> {
+
+    const client = df.getClient(context);
+
+    // (re)using just one orchestration instance
+    const instanceId = 'my-repka-instance';
+
+    var repka = DefaultRepka;
+
+    var instanceStatus: any = await client.getStatus(instanceId);
+    if (!instanceStatus) {
+
+        // Starting a new repka
+        repka = DefaultRepka;
+
+    } else if(!instanceStatus.output) {
+
+        return {
+            status: 500,
+            body: 'Repka is being reversed. Please, wait.'
+        };
+
+    } else {
+        // Reversing existing repka
+        repka = instanceStatus.output;
+
+        repka.left = repka.right;
+        repka.right = null;
+    }
+
+    await client.startNew('the-saga-of-repka', instanceId, repka);
+
+    return { body: "The saga of Repka has (re)started" };
+};
+
+const DefaultRepka = {
+    right: null,
+    left: {
+        name: "dedka",
+        next: {
+            name: "babka",
+            next: {
+                name: "vnuchka",
+                next: {
+                    name: "zhuchka",
+                    next: {
+                        name: "koshka",
+                        next: {
+                            name: "myshka",
+                            next: null
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
---
+++ the-saga-of-repka/index.ts
@@ -0,0 +1,24 @@
+﻿import * as df from "durable-functions"
+
+export default df.orchestrator(function* (context) {
+
+    const repka: any = context.df.getInput();
+    context.df.setCustomStatus(repka);
+
+    // Reversing the Repka chain
+    while (repka.left) {
+
+        // Adding timer for more suspense
+        yield context.df.createTimer(new Date(context.df.currentUtcDateTime.getTime() + 3000));
+
+        const temp = repka.right;
+        repka.right = repka.left;
+        repka.left = repka.left.next;
+        repka.right.next = temp;
+
+        context.df.setCustomStatus(repka);
+    }
+
+    return repka;
+});
+
